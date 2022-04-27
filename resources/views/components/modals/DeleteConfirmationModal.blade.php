1b8a77f72d8c63a5a033892553e53e39879f5a08
ago-dev <dev.devisago@gmail.com>
2022-04-27 17:38:28 +0200

    UI changes

---
+++ resources/views/components/modals/DeleteConfirmationModal.blade.php
@@ -0,0 +1,21 @@
+<div class="modal fade" id="employee-delete-confirmation-modal" tabindex="-1" role="dialog" aria-labelledby="employee-delete-confirmation-modal-label" aria-hidden="true">
+   <form role="form" method="POST">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title font-weight-bold fa-1x" id="exampleModalLabel">Delete Employee</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this employee?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-success" data-dismiss="modal">No</button>
+                <button type="button" class="btn btn-danger">Yes</button>
+            </div>
+        </div>
+    </div>
+   </form>
+</div>
